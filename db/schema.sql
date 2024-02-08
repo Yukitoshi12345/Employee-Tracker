4c4db7df0a3a3adb34c47238d39351e398a4d488
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-09 00:52:32 +1100

    defined data type and constraints in department, row, employee table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,23 @@ DROP DATABASE IF EXISTS Employee_Tracker_db;
 CREATE DATABASE Employee_Tracker_db;
 
 USE Employee_Tracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE row (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10,2) NOT NULL,
+    dept_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
