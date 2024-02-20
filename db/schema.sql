57de3c994c3bbb0242e05772ebcbc4b2d7a0b66b
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-02-20 15:23:33 +1100

    schema.sql completed

--- db/schema.sql
+++ db/schema.sql
@@ -19,8 +19,11 @@ CREATE TABLE department (
 CREATE TABLE row (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INTEGER
+    salary DECIMAL(10,2),
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 -- Creates a table named "employee"
@@ -29,5 +32,11 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
