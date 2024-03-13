9b85460a70a09edd4be9249be5d23620d737883b
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-13 23:40:13 +1100

    added tables files

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require("inquirer");
 const mysql = require(`mysql2`);
 const ctable = require(`console.table`);
 
+const viewEmployee = require("./tables/view/viewEmployee");
+
 require("dotenv").config();
 
 const db = mysql.createConnection({
@@ -59,3 +61,27 @@ function selectQuestion() {
       }
     });
 }
+
+function viewDepartment() {
+  console.log("Viewing all Departments in database");
+  //query to select name and id from department
+  // Structures the department table
+  db.query(
+    `SELECT department_name AS "Department Name", id AS "Department ID" FROM departments`,
+    (error, departments) => {
+      if (error) {
+        console.log("Failed to find departments", error);
+        return;
+      }
+      //Checking if there are no departments
+      if (departments.length === 0) {
+        console.log("No departments found");
+      } else {
+        // displays table for all departments
+        console.table(departments);
+      }
+    }
+  );
+}
+
+module.exports = selectQuestion;
