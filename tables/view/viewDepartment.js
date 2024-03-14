204bd00caf291296225530f5d315eebdb1fc6745
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-15 02:01:26 +1100

    fixed viewDepartment

--- tables/view/viewDepartment.js
+++ tables/view/viewDepartment.js
@@ -1,5 +1,5 @@
 function viewDepartment(db, selectQuestion) {
-  const departmentTable = `SELECT * FROM departments`;
+  const departmentTable = `SELECT d.id, d.department_name AS "Department Name" FROM departments d`;
   db.query(departmentTable, (err, results) => {
     if (err) {
       console.error(err);
