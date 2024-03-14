76391b789cc531ccb6acf45d9a84a855fac176fe
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-15 01:02:18 +1100

    completed view Employee

--- tables/view/viewEmployee.js
+++ tables/view/viewEmployee.js
@@ -1,6 +1,6 @@
 // Displays the name, title, salary, department, and manager for each employee
 function viewEmployee(db, selectQuestion) {
-  const sql = `SELECT e.id, e.first_name, e.last_name, r.title, r.salary AS Salary, d.department_name AS Department, 
+  const sql = `SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title AS Title, r.salary AS Salary, d.department_name AS Department, 
     CONCAT(m.first_name, ' ', m.last_name) AS Manager
     FROM employees e
     LEFT JOIN roles r ON e.role_id = r.id
