f0e58b69039a450182538f67ec57c04681e55292
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-16 12:09:11 +1100

    fixed manager to employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,28 +9,28 @@ VALUES ("Marketing"),
 
 -- Insert role records with titles, salaries, and department IDs       
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Digital Marketing Manager", "110000", 001),
-       ("Content Marketing Specialist", "87500", 001),
-       ("Senior Software Developer", "129471", 002),
-       ("Junior Software Developer","85000", 002),
-       ("Human Resources Director", "160000", 003),
-       ("Human Resources Generalist", "90000", 003),
-       ("Financial Controller", "150000", 004),
-       ("Accountant", "90000", 004),
-       ("Supply Chain Manager", "137863", 005),
-       ("Inventory Control Specialist", "81182", 005);
+VALUES ("Digital Marketing Manager", "110000", 1),
+       ("Content Marketing Specialist", "87500", 1),
+       ("Senior Software Developer", "129471", 2),
+       ("Junior Software Developer","85000", 2),
+       ("Human Resources Director", "160000", 3),
+       ("Human Resources Generalist", "90000", 3),
+       ("Financial Controller", "150000", 4),
+       ("Accountant", "90000", 4),
+       ("Supply Chain Manager", "137863", 5),
+       ("Inventory Control Specialist", "81182", 5);
 
 -- Insert employee records with names, role IDs, and manager IDs     
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Violet", "Nelson", 001, NULL),
-       ("Mason", "Harris", 002, 001),
-       ("Mila", "Lee", 003, NULL),
-       ("Liam", "Young", 004, 002),
-       ("Sophia", "Garcia", 005, NULL),
-       ("James", "Hernandez", 006, 003),
-       ("Harper", "Taylor", 007, NULL),
-       ("Daniel", "Walker", 008, 004),
-       ("Aiden", "Smith", 009, NULL),
-       ("Amelia", "Miller", 010, 005);
+VALUES ("Violet", "Nelson", 1, NULL),
+       ("Mason", "Harris", 2, 1),
+       ("Mila", "Lee", 3, NULL),
+       ("Liam", "Young", 4, 3),
+       ("Sophia", "Garcia", 5, NULL),
+       ("James", "Hernandez", 6, 5),
+       ("Harper", "Taylor", 7, NULL),
+       ("Daniel", "Walker", 8, 7),
+       ("Aiden", "Smith", 9, NULL),
+       ("Amelia", "Miller", 10, 9);
 
     
